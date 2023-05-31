a35b15e7e39a1447cc0a3e705f8b1f7efbfe7997
m0nst4ez <eziomahesa14@gmail.com>
2023-05-31 12:37:49 +0800

    Selesai pertemuan 3

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// error_reporting(0);
+include 'header.php';
+
+//untuk membuat tampilan menjadi dinamis
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+    switch ($page) {
+        case "home":
+            include 'home.php';
+            break;
+
+        case "profile":
+            include 'profile.php';
+            break;
+
+        case "login":
+            include 'login.php';
+            break;
+
+        default:
+            include 'home.php';
+            break;
+    }
+} else {
+    include 'home.php';
+}
+include 'footer.php';
---
+++ profile.php
@@ -0,0 +1,54 @@
+<?php  ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/Mahesa.jpg" alt="Dedy Rosyadi" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM:</strong></td>
+                        <td align="left" width="50%">2110010062</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Mahesa</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 14 juli 2003</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">089501335227</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">eziomahesa14@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com" target="_blank" style="text-decoration:none;">
+                                <img src="img/facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com" target="_blank" style="text-decoration:none;">
+                                <img src="img/instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com" target="_blank" style="text-decoration:none;">
+                                <img src="img/twitter.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com" target="_blank">
+                                <img src="img/linked.svg" width="30" alt="linked">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
