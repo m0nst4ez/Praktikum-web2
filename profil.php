e2cedf8ce7b1dd8166982f1a04c27e29c54d412a
m0nst4ez <134354091+m0nst4ez@users.noreply.github.com>
2023-07-04 11:25:48 +0800

    Add files via upload

---
+++ profil.php
@@ -0,0 +1,45 @@
+<?php include "header.php" ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/Mahesa.jpg" alt="Mahesa" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Mahesa</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2110010062</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 14 Juli 2003</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">089501335227</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Email :</strong></td>
+                        <td align="left" width="50%">eziomahesa14@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            </a>
+                            <a href="https://instagram.com/abcdefgjhijklmnopqrstuvwxyz_" target="_blank" style="text-decoration:none;">
+                                <img src="img/instagram.svg" width="30" alt="instagram">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
